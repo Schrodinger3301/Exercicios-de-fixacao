ca6ed25a3ffc3fceca4db2aad4ca1c9a38dd5559
Ruan Maia <ruanfarias.maiabarbosa@gmail.com>
2026-03-31 16:33:02 -0400

    EQUIPE BLACKHOLE

---
+++ App.js
@@ -0,0 +1,92 @@
+/* EQUIPE BLACKHOLE
+Ruan
+André
+Janaina
+*/ 
+//Variaveis constantes
+const nome = "joão";
+var idade =30
+/*com concatenação "+"
+interpolação de tipos */
+
+//if/else
+
+//AND OR NOT 
+
+//SWITCH CASE
+
+/*1- Carregar o celular 
+Analogia:Você so irá carregar o celular se o cabo estiver conectado 
+e se o carregador estiver na tomada.*/
+//if/else
+    //com concatenação "+"
+
+if(CaboConentado && CaboNaTomada) {
+    console.log(nome + "O Celular está carregando")
+}else{
+    console.log(nome + "O Celular não está carregando")
+}
+    //com interpolação
+if (CaboConentado && CaboNaTomada) {
+    console.log(`${nome} O Celular está carregando `)
+}else{
+    console.log(`${nome} O Celular não está carregando `)
+}
+//AND
+let CaboConentado2 = true;
+let CaboNaTomada2 = true;
+
+if(CaboConentado2 && CaboNaTomada2) {
+    console.log("O Celular está carregando")
+}else{
+    console.log("O Celular não está carregando")
+}
+//OR 
+let CaboConentado3 = false;
+let CaboNaTomada3 = true;
+if(CaboConentado3 || CaboNaTomada3) {
+    console.log(nome + "O Celular está carregando")
+}else{
+    console.log(nome + "O Celular não está carregando")
+}
+//NOT 
+let CaboConentado4 = false
+let CaboNaTomada4 = false
+if(CaboConentado4 || !CaboNaTomada4) {
+    console.log(nome + "O Celular está carregando")
+}else{
+    console.log(nome + "O Celular não está carregando")
+}
+//SWITCH CASE
+
+
+
+/*2- fazer um sanduiche 
+Analogia:O sanduiche so será pronto se tiver pão e tiver recheio*/
+//if/else
+
+//AND OR NOT 
+
+//SWITCH CASE
+
+
+
+
+/*3- Entrar em uma festa
+Analogia: Você so poderá entra na festa se for maior de 18 anos e tiver identidade.*/
+//if/else
+
+//AND OR NOT 
+
+//SWITCH CASE
+
+
+
+
+/*4- Dirigir um carro 
+Analogia: Você so poderá dirigir se for habilitado e tiver com o documento em mãos*/
+//if/else
+
+//AND OR NOT 
+
+//SWITCH CASE
